00b4f73b74d11c80724b8caae47085d359988dcc
Sajal Talukder <sajaltalukder01@gmail.com>
2025-09-23 20:36:30 +0600

    guardfix

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -10,55 +10,10 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-// import { BASE_API_URL } from "@/server";
 
-// import axios from "axios";
-
-import { Loader, MenuIcon } from "lucide-react";
-
-// import { redirect } from "next/navigation";
-// import { useEffect, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { handleAuthRequest } from "../utils/apiRequest";
-// import { setAuthUser } from "@/store/authSlice";
-
-// import { RootState } from "@/store/store";
+import { MenuIcon } from "lucide-react";
 
 const Home = () => {
-  // const dispatch = useDispatch();
-  // const user = useSelector((state: RootState) => state.auth.user);
-  // const [isLoading, setIsLoading] = useState(false);
-
-  // useEffect(() => {
-  //   const getAuthUser = async () => {
-  //     const getAuthUserReq = async () =>
-  //       await axios.get(`${BASE_API_URL}/users/me`, {
-  //         withCredentials: true,
-  //       });
-  //     const result = await handleAuthRequest(getAuthUserReq, setIsLoading);
-
-  //     if (result) {
-  //       dispatch(setAuthUser(result.data.data.user));
-  //     }
-  //   };
-  //   getAuthUser();
-  // }, [dispatch]);
-
-  // useEffect(() => {
-  //   console.log("USER inside USEEFFECT", user);
-  //   if (!user) return redirect("/auth/login");
-  // }, [user]);
-
-  // console.log("USER OUTSIDE USEEFFECT", user);
-
-  // if (isLoading) {
-  //   return (
-  //     <div className="w-full h-screen flex items-center justify-center flex-col">
-  //       <Loader className="animate-spin" />
-  //     </div>
-  //   );
-  // }
-
   return (
     <div className="flex  ">
       <div className="w-[20%] hidden md:block border-r-2 h-screen fixed ">
